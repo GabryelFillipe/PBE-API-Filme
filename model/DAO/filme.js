a90944df442816f067d1d46a48dbee5c08a2d625
GABRYEL FILLIPE CAVALCANTI DA SILVA <25132404@CFP127ALUNO.LOCAL>
2025-10-07 11:31:54 -0300

    Atualizando

--- model/DAO/filme.js
+++ model/DAO/filme.js
@@ -28,7 +28,7 @@
  */
 
 // Import da dependencia do prima que permite a execução de script SQL no BD
-const { PrismaClient } = require('@prisma/client')
+const { PrismaClient } = require('../../generated/prisma')
 
 // Cria um novo objeto baseado na classe do PrismaClient
 const prisma = new PrismaClient()
